79003bb1d67196308961238fe11e31de8cb5ba4d
jhordan-vaz <jhordanX@gmail.com>
2023-05-13 14:28:59 -0300

    Udemy-Course
    .

---
+++ introducao/aula19.js
@@ -0,0 +1,25 @@
+/*function saudacao(nome) {
+  return `Bom dia ${nome}!`;
+}
+
+const variavel = saudacao("Jhordismar");
+console.log(variavel);
+
+//saudacao("Jhordan");
+//saudacao("Maria");
+
+function soma(x = 0, y = 0) {
+  const resultado = x + y;
+  return resultado;
+}
+
+console.log(soma(2, 4));
+console.log(soma(8, 6));
+*/
+
+const raiz = function (n) {
+    return n ** 0.5;
+};
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
