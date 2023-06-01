c80a5734b2b883eb8325828a319652fe7c9af56d
jhordan-vaz <jhordanX@gmail.com>
2023-06-01 01:05:08 -0300

    Udemy-Course
    .

---
+++ exercicios/ex39.js
@@ -0,0 +1,14 @@
+// Escreva uma função que recebe 2 numeros e retorne o maior deles.
+
+function max(x, y) {
+  return x > y ? x : y; // se X for maior que Y retorna X se não Y
+ }
+ 
+// const max2 = (x, y)  => x > y ? x : y;
+
+console.log(max(10,9 ));
+
+
+
+
+
---
+++ exercicios/nodeList/assets/js/main.js
@@ -0,0 +1,13 @@
+const paragrafos = document.querySelector(".paragrafos");
+const ps = document.querySelectorAll("p");
+
+const estilosBody = getComputedStyle(document.body);
+const backGroundColorBody = estilosBody.backgroundColor;
+console.log(backGroundColorBody);
+
+for (let p of ps) {
+  p.style.backgroundColor = backGroundColorBody;
+  p.style.color = "#FFFFFF";
+}
+
+
