f2d175b711dc9151db4d3b697ce5e5490a1f1f18
jhordan-vaz <jhordanX@gmail.com>
2023-07-05 01:59:41 -0300

    Udemy-Course
    .

--- Object/Aula78.js
+++ Object/Aula78.js
@@ -1,2 +1,34 @@
 // Factory Function + Proto
 
+const falar = {
+  falar(){
+    console.log(`${this.nome} está falando.`);
+  },
+}
+
+const vaping = {
+  vaping() {
+    console.log(`${this.nome} está fumando.`);
+  },
+}
+
+const jogando = {
+  jogando(){
+    console.log(`${this.nome} está jogando.`);
+  },
+}
+   const pessoaPrototype = Object.assign({}, falar, vaping, jogando);
+// const pessoaPrototype = {...falar, ...vaping};
+
+function criaPessoa(nome, sobrenome,  ) {
+  return Object.create(pessoaPrototype, {
+    nome: { value: nome },
+    sobrenome: {value: sobrenome },
+  });
+}
+
+
+
+const p1 = criaPessoa("Jhordan", "Vaz");
+console.log(p1);
+p1.falar();
