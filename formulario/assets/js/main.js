69db9a671a880a59630c14e4b7d1d14520f740ce
jhordan-vaz <jhordanX@gmail.com>
2023-07-11 22:16:05 -0300

    Udemy-Course
    Formulário em classe.

--- formulario/assets/js/main.js
+++ formulario/assets/js/main.js
@@ -1,13 +1,27 @@
 class ValidaFormulario {
     constructor() {
-        
-        
+        this.formulario = document.querySelector(".formulario");
         this.eventos();
     }
 
     eventos() {
+        this.formulario.addEventListener("submit", e => {
+          this.handleSubmit(e);
+        });
+    }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        const validFields = this.checkFields();
+    }
+
+    checkFields() {
+      let valid = true;
+
+      for(let field of this.formulario.querySelectorAll(".valid"))  {
+        console.log(field)
+      }
     }
 }
 
-const valida = ValidaFormulario();
+const valida = new ValidaFormulario();
