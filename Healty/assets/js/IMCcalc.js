e6cb64658ece90ae2ace4d83cbc8e2ffe526816e
jhordan-vaz <jhordanX@gmail.com>
2023-07-13 21:07:04 -0300

    Udemy-Course
    .

---
+++ Healty/assets/js/IMCcalc.js
@@ -0,0 +1,14 @@
+class validaIMC {
+  constructor(sentIMC) {
+    Object.defineProperty(this, "imcLimpo", {
+      writable: true,
+      enumerable: true,
+      configurable: false,
+    });
+  }
+}
+
+getIMC(imc) {
+  
+}
+
