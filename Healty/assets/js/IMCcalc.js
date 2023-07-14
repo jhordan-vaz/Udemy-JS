b2be89c9dfcf8e2f621a471e601201a218328545
jhordan-vaz <jhordanX@gmail.com>
2023-07-14 20:00:48 -0300

    Udemy-Course
    .

--- Healty/assets/js/IMCcalc.js
+++ Healty/assets/js/IMCcalc.js
@@ -1,14 +1,21 @@
 class validaIMC {
-  constructor(sentIMC) {
-    Object.defineProperty(this, "imcLimpo", {
+  constructor(height, weight) {
+    Object.defineProperty(this, "imc", {
       writable: true,
       enumerable: true,
       configurable: false,
     });
   }
+
+  getIMC(height, weight) {
+    const calcIMC = this.imc;
+  }
+  
+  static calcIMC(height, weight) {
+    let imc = height / (weight * weight);
+  }
+
 }
 
-getIMC(imc) {
-  
-}
+let totalIMC = new validaIMC(1.80, 80);
 
