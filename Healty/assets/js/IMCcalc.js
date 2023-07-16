b29148dec8a7305aa520ee0472acf8a55153d76e
jhordan-vaz <jhordanX@gmail.com>
2023-07-16 20:52:14 -0300

    Udemy-Course
    Treinando com classe/Function.

--- Healty/assets/js/IMCcalc.js
+++ Healty/assets/js/IMCcalc.js
@@ -1,21 +1,31 @@
-class validaIMC {
-  constructor(height, weight) {
-    Object.defineProperty(this, "imc", {
-      writable: true,
-      enumerable: true,
-      configurable: false,
-    });
-  }
+// class validaIMC {
+//   constructor(imc) {
+//     Object.defineProperty(this, "imc", {
+//       writable: true,
+//       enumerable: true,
+//       configurable: false,
+//     });
+//   }
 
-  getIMC(height, weight) {
-    const calcIMC = this.imc;
-  }
-  
-  static calcIMC(height, weight) {
-    let imc = height / (weight * weight);
-  }
 
+//    calcIMC(height, weight) {
+//     let imc = height / (weight * weight);
+//   }
+
+// }
+
+// let totalIMC = new validaIMC(1.80, 80);
+
+function CalcIMC(peso, altura){
+    this.peso = peso
+    this.altura = altura
+
+    this.calcIMC = function() {
+      let imc;
+      imc = this.peso / ( this.altura * this.altura);
+      console.log(imc);
+  };
 }
 
-let totalIMC = new validaIMC(1.80, 80);
-
+const imc1 = new CalcIMC(80, 1.80);
+imc1.calcIMC();
