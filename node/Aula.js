7a08e61c63114da0696421ca2583d12fe957c84f
jhordan-vaz <jhordanX@gmail.com>
2023-07-22 20:52:32 -0300

    Udemy-Course
    NPM commands.

---
+++ node/Aula.js
@@ -0,0 +1,8 @@
+// npm unistall express
+// npm instal app E
+// npm i --save-dev | development
+// npm i --save-prod | production
+// npm outdated
+// npm i app 4x
+// npm update
+// npm i -E (version)
