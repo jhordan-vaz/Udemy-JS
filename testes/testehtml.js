4990abbe10bf0a1fd183be7f024bd2d409281b0a
jhordan-vaz <jhordanX@gmail.com>
2023-07-27 14:57:34 -0300

    Udemy-Course
    Revisando.

---
+++ testes/testehtml.js
@@ -0,0 +1,10 @@
+function Person(name, lastName, weight, height) {
+  this.name = name;
+  this.lastName = lastName;
+  this.weight = weight;
+  this.height = height;
+}
+
+function getImc() {
+  
+}
