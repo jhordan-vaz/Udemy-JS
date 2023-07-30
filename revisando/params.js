c34fe3c12ce1cdb3c4f6fa42e04692c4ddc17062
jhordan-vaz <jhordanX@gmail.com>
2023-07-29 21:53:14 -0300

    Udemy-Course
    .

--- revisando/params.js
+++ revisando/params.js
@@ -1,10 +1,18 @@
-function funcao() {
+// Argumentos são valores enviados aqui
+function funcao(a, b, c) { // parametros são dentro da funcao
   let total = 0;
   for (let argumento of arguments) {
     total += argumento;
   }
   
-  console.log(total);
+  console.log(total, a, b, c)
+  
 }
 funcao(1, 2, 3, 4 , 5);
+
+
+function funcao2(a, b, c, d, e, f) {
+  console.log(a, b, c, d, e, f);
+}
+funcao2(1, 2, 3);
 
