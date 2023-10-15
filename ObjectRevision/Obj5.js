9fd29e85f005c4cac46ac3e5db9242d8f7dc7100
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-10-14 23:55:26 -0300

    Revisnado Obj
    .

--- ObjectRevision/Obj5.js
+++ ObjectRevision/Obj5.js
@@ -1,1 +1,13 @@
 // Prototype
+
+function Pessoa(nome, sobrenome) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+}
+
+Pessoa.prototype.falarNome = function () {
+    return `${this.nome} ${this.sobrenomenome}`
+}
+
+const p1 = Pessoa("Jhordan", "Vaz");
+console.log(p1);
