d6eba0e9bf27b52320c74c7f8fff12a5536a4e82
Jhordan <93515036+jhordan-vaz@users.noreply.github.com>
2023-10-31 23:24:54 -0300

    Async
    .

--- Promisses/reqHtml/script.js
+++ Promisses/reqHtml/script.js
@@ -43,53 +43,54 @@
 
 // forma atual 
 
-const request = obj => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send(null);
+// const request = obj => {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(obj.method, obj.url, true);
+//     xhr.send(null);
 
-    xhr.addEventListener("load", () => {
-       if(xhr.status >= 200 && xhr.status < 300) {
-         resolve(xhr.responseText);
-       } else {
-         reject(xhr.statusText);
-       }
-    });  
-    })
+//     xhr.addEventListener("load", () => {
+//        if(xhr.status >= 200 && xhr.status < 300) {
+//          resolve(xhr.responseText);
+//        } else {
+//          reject(xhr.statusText);
+//        }
+//     });  
+//     })
     
-};
+// };
 
-document.addEventListener("click", e => {
-    const el = e.target;
-    const tag = el.tagName.toLowerCase();
+// document.addEventListener("click", e => {
+//     const el = e.target;
+//     const tag = el.tagName.toLowerCase();
     
-    if(tag === "a") { 
-        e.preventDefault();
-        loadPage(el);
-      }
-});
+//     if(tag === "a") { 
+//         e.preventDefault();
+//         loadPage(el);
+//       }
+// });
 
 
 
-async function loadPage(el) {
-    const href = el.getAttribute("href");
+// async function loadPage(el) {
+//     const href = el.getAttribute("href");
    
-    const objConfig = {
-        method: "GET", 
-        url: href, 
-    };
- try {
-    const response = await request(objConfig);
-      load(response);
-  } catch(e) {
-      console.log(e);
-}
+//     const objConfig = {
+//         method: "GET", 
+//         url: href, 
+//     };
+//  try {
+//     const response = await request(objConfig);
+//       load(response);
+//   } catch(e) {
+//       console.log(e);
+// }
   
     
-}
+// }
 
-function load(response) {
-    const resultado = document.querySelector(".resultado");
-    resultado.innerHTML = response;
-}
+// function load(response) {
+//     const resultado = document.querySelector(".resultado");
+//     resultado.innerHTML = response;
+// }
+
